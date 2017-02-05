524c13889467c6c64f313c8accb9d9d2bc705b4b
Scott Lawson <scottlawsonbc@gmail.com>
2017-02-05 05:29:48 -0800

    Add AI.fs module for the chess engine AI

---
+++ ChessEngine/AI.fs
@@ -0,0 +1,39 @@
+﻿namespace ChessEngine.Core
+
+// Handles the artificial intelligence for the CPU player
+module AI =   
+    
+    open System
+    open System.Collections.Generic
+    
+    /// Opening moves used by the AI
+    type OpeningMove(startFen, endFen) = 
+        let mutable endingFEN = startFen // ending Forsyth-Edwards notation
+        let mutable startingFEN = endFen // starting Forysyth-Edwards notation
+        let mutable moves = new List<MoveContent>()
+        let mutable currentGameBook = new List<OpeningMove>() // Holds a record of all game moves
+
+        member x.EndingFEN 
+            with get() = endingFEN
+            and set value = endingFEN <- value
+
+        member x.StartingFEN
+            with get() = startingFEN
+            and set value = startingFEN <- value
+
+        member x.Moves
+            with get() = moves
+            and set value = moves <- value
+
+        member x.CurrentGameBook 
+            with get() = currentGameBook
+            and set value = currentGameBook <- value
+
+        new () = new OpeningMove("", "")
+
+        member x.SaveCurrentGameMove (board:Board) (previousBoard:Board) (gameBook:ICollection<OpeningMove>) (bestMove:MoveContent) =
+            try
+                let move = 
+                move.star
+            with
+            | ex -> failwith ex.Message
